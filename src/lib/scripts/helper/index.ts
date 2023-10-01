7ceb870162306ddc51faf347c8ba393346590c4e
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-10-01 02:02:23 +0200

    feat: :construction: Introduced some helper functions

--- src/lib/scripts/helper/index.ts
+++ src/lib/scripts/helper/index.ts
@@ -1,27 +1,52 @@
-import { setItemValue } from "../localStorage"
+import type { ContactDetails } from "$lib/types";
+import { setItemValue } from "../localStorage";
+import { saveAs } from "file-saver";
+import { v4 as uuidv4 } from "uuid";
+import vCardsJS from "vcards-js";
 
-export const toggleUserGuide = (state:boolean) : boolean => {
-    setItemValue('show-notice', !state)
-    return !state
-}
+export const toggleUserGuide = (state: boolean): boolean => {
+  setItemValue("show-notice", !state);
+  return !state; 
+};
 
-export const isValidName = (name: string = '') : boolean => {
-    const re = /^[^0-9]+$/g;
-    const found : boolean = re.test(name);
-    return found
-} 
-export const isValidPhone = (phone: string = '') : boolean => {
-   const re = /^[0-9]+$/g;
-    const found : boolean = re.test(phone);
-    return found
-} 
+export const isValidName = (name: string = ""): boolean => {
+  const re = /^[^0-9]+$/g;
+  const found: boolean = re.test(name);
+  return found;
+};
+export const isValidPhone = (phone: string = ""): boolean => {
+  const re = /^[0-9]+$/g;
+  const found: boolean = re.test(phone);
+  return found;
+};
 
 export const sanitizer = (dirtyString: string): string => {
-    let cleansedString: string = dirtyString
-    const badChars : string = '^?/"!@#$%&*()_+-=><,.;:{}[]\'|'
-    for (let i = 0; i < badChars.length-1; i++){
-        cleansedString = cleansedString.replaceAll(badChars[i], "");
+  let cleansedString: string = dirtyString;
+  const badChars: string = "^?/\"!@#$%&*()_+-=><,.;:{}[]'|";
+  for (let i = 0; i < badChars.length - 1; i++) {
+    cleansedString = cleansedString.replaceAll(badChars[i], "");
+  }
+  return cleansedString;
+};
 
-    }
-    return cleansedString
+export const saveVcfFile = (vcfData: string) => {
+  try {
+    const blob = new Blob([vcfData], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, `vCard_0${uuidv4(10)}.vcf`);
+  } catch (err) {
+   return err
+  }
+};
+
+export const createVcard = (data: ContactDetails[], batch:string): string => {
+    const vCard = vCardsJS();
+    let finalVcard = "";
+    data.forEach((e) => {
+      vCard.cellPhone = e.phone;
+      vCard.firstName = `wabuzz-${batch}` + e.name;
+      finalVcard = finalVcard + vCard.getFormattedString();
+    });
+
+    return finalVcard
+    
 }
--- src/lib/scripts/helper/toaster/index.ts
+++ src/lib/scripts/helper/toaster/index.ts
@@ -1,11 +1,11 @@
 import toast from "svelte-french-toast";
 
-export function addToast(message:string, type:string, duration = 4500) {
+export function addToast(message:string, type:string, duration = 3500) {
   const config = {
     duration,
 
     style:
-      "border-radius: 10px; font-size: 14px !important; background: #FFF; color: #555;",
+      "border-radius: 10px; font-size: 14px !important; background: #FFF; color: #555; font-weight: 700;",
   };
   if (type === "success") {
     toast.success(message, config);
